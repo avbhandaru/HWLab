d4175648301bc21eea1e5890d2d3092485d2f0b6
AkhilB <avb39@cornell.edu>
2018-05-31 14:37:54 -0400

    myar

---
+++ test.py
@@ -0,0 +1,18 @@
+# This is a testing suite for a pseudo ARMv7 cpu made using logisim
+
+import math
+import random
+
+tests = open("ARMv7.txt", 'w')
+tests.write("This is a testing suite for a pseudo ARMv7 cpu made using logisim\n")
+
+
+
+
+
+def test():
+    
+
+
+
+tests.close()
