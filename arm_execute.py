5ff6fdf3bb0078cbfc5c3379e8607a7822f6d226
AkhilB <avb39@cornell.edu>
2018-06-12 12:02:13 -0400

    meow

--- arm_execute.py
+++ arm_execute.py
@@ -5,57 +5,63 @@ import re
 # <opcode>{<cond>}{S} <Rd>, <Rn>, <shifter_operand>
 # <opcode>{<cond>}{Br} <branch_immediate>
 
-tests = open("RegisterFileARM.txt", 'w')
-tests.write("@ This is an execution suite for a pseudo ARMv7 cpu made using logisim @\n")
+file = open("TestARM.txt", 'r')
 
-DataProcessing = {
-    0: "AND", 1: "EOR", 2: "SUB", 3: "RSB", 4: "ADD", 5: "ADC", 6: "SBC", 7: "RSC",
-    8: "TST", 9: "TEQ", 10: "CMP", 11: "CMN", 12: "ORR", 13: "MOV", 14: "BIC", 15: "MVN"
-}
+def fetch(line):
+    """ Fetches and sends the first line of the TestArm.txt file to
+    the next stage of the pipeline. Requires formatting and a few
+    other commands to get it into a string format."""
+    print "fetched"
 
-Shifts = {
-    0: "LSL", 1: "LSR", 2: "ASR", 3: "ROR", 4: "RRX"
-}
 
-DataTransfer = {
-    0: "LDR", 1: "LDRB", 2: "LDRSB", 3: "STR", 4: "STRB"
-}
+def decode(line):
+    """  """
 
-Branches = {
-    0: "B", 1: "BL", 2: "BX", 3: "BLX"
-}
 
-Conditionals = {
-    0: "EQ", 1: "NE", 2: "HS", 3: "LO", 4: "MI", 5: "PL", 6: "VS", 7: "VC",
-    8: "HI", 9: "LS", 10: "GE", 11: "LT", 12: "GT", 13: "LE", 14: "AL", 15: "NV"
-}
+def execute(line):
+    """ """
+    return 0
 
-Registers = {
-    0: "a1", 1: "a2", 2: "a3", 3: "a4", 4: "v1", 5: "v2", 6: "v3", 7: "v4",
-    8: "v5", 9: "v6", 10: "v7", 11: "v8", 12: "ip", 13: "sp", 14: "lr", 15: "pc",
-    16: "cpsr"
-}
 
-"""  """
-def compileInstruction():
-    
+def memory(line):
+    """ """
+    return 0
 
-""" """
-def executeInstruction(num):
-    while i < num:
-        # Updates Register File and RAM Memory files
-        i += 1
 
-""" """
+def writeback(line):  
+    """ """
+    return 0
+
+
+def run(inst):
+    """ """
+    fetch(line)
+    decode(line)
+    execute(line)
+    memory(line)
+    writeback(line)
+    update()
+
+
+def update():
+    """ updates registers and memory access files """
+
+
 def showRegisters():
+    """ """
+    return 0
 
 
-""" """
 def showRAMAccess():
+    """ """
+    return 0
 
 
+# Scripts
+for line in file:
+    run(line)
 
-tests.close()
+file.close()
 
 """ 
 ## Data Processing ##
